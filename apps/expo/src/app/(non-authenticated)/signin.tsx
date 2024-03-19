fbf9d54d613af09572cc417671daf1d14b69d738
Viktor Malmedal <viktor@malmedal.dev>
2024-03-19 16:25:31 +0100

    trpc error link

---
+++ apps/expo/src/app/(non-authenticated)/signin.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SigninView() {
+  return (
+    <View>
+      <Text>sign in view</Text>
+    </View>
+  );
+}
--- apps/expo/src/lib/trpc.tsx
+++ apps/expo/src/lib/trpc.tsx
@@ -1,8 +1,11 @@
 import type { AppRouter } from "@snuber/api/trpc";
+import type { TRPCLink } from "@trpc/client";
 import React from "react";
+import { router } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
+import { observable } from "@trpc/server/observable";
 import superjson from "superjson";
 
 import { getBaseAPIUrl } from "~/utils/get-base-url";
@@ -10,11 +13,34 @@ import { secureStore } from "~/utils/secure-store";
 
 export const trpc = createTRPCReact<AppRouter>();
 
+const errorLink: TRPCLink<AppRouter> = () => {
+  return ({ next, op }) => {
+    return observable((observer) => {
+      const unsubscribe = next(op).subscribe({
+        next(value) {
+          observer.next(value);
+        },
+        error(err) {
+          observer.error(err);
+          if (err.data?.code === "UNAUTHORIZED") {
+            router.replace("/(non-authenticated)/signin");
+          }
+        },
+        complete() {
+          observer.complete();
+        },
+      });
+      return unsubscribe;
+    });
+  };
+};
+
 export function TRPCProvider(props: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
   const [trpcClient] = React.useState(() =>
     trpc.createClient({
       links: [
+        errorLink,
         loggerLink({
           enabled: (opts) =>
             process.env.NODE_ENV === "development" ||
